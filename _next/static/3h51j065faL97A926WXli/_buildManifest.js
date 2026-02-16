cad449757ece0db33f6987d51da391c84970530b
Borodkina Viktoriia <Motiviruisya@yandex.ru>
2026-02-16 12:05:22 +0300

    Updates

--- _next/static/3h51j065faL97A926WXli/_buildManifest.js
+++ _next/static/3h51j065faL97A926WXli/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/akvahold//_next/:path+",
-        "destination": "/akvahold/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
